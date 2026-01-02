0ddc00a5688c99647a0078ab2aa469ed9ae076e5
Mx-Iris <mxiris@126.com>
2026-01-03 02:40:29 +0800

    Improve performance

--- Package.swift
+++ Package.swift
@@ -74,7 +74,7 @@ extension SwiftSetting {
     static let globalConcurrency: Self = .enableUpcomingFeature("GlobalConcurrency")                          // SE-0412, Swift 5.10, SwiftPM 5.10+
 }
 
-extension SwiftSetting: CaseIterable {
+extension SwiftSetting: @retroactive CaseIterable {
     public static var allCases: [Self] {
         [
             .forwardTrailingClosures,
